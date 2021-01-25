27e3424632d455cffc56b7e0333d2fdf3f7df8a3
SudarshanKumar <sudarshan4309@gmail.com>
2021-01-25 12:06:25 +0530

    sczxc

--- index.js
+++ index.js
@@ -1,31 +1,105 @@
-const express = require('express'); 
-const bodyParser = require('body-parser'); // middleware
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
 
-
-const app = express(); // Create an ExpressJS app
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/static/index.html');
-  });
+
+// parse application/json
+app.use(bodyParser.json());
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.set('view engine','ejs');
+app.get('/login',(req,res)=>{
+    res.render('index',{
+        action: "sign In",
+        loggedIn : true
+                });
+});
+
+app.get('/consent', (req,res) => {
+    res.render('consent', {appName: "SudApp" , })
+});
 
 
-  app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/static/login.html');
-  });
+app.post('/consent', (req,res) => {
+        console.log("consent granted");
+
+        if (req.body.submit != 'yes') {
+            console.log('user has declined to grant permission');
+            // ! request.body.redirect_uri.startsWith('oob') &&
+            // ! request.body.redirect_uri.startsWith('urn:ietf:wg:oauth:2.0:oob')
+            console.log('BODY: ' + JSON.stringify(req.body.email));
+            // res.render('validate');
+    
+            res.status(302)
+              .header('Location', 'https://www.google.com'+ '?error=access_denied')
+              .end();
+            return;
+          }
+        else{
+            console.log("sign ");
+            console.log(req.body.email);
+            console.log(req.body.pwd);
+            if ( ! req.body.email || ! req.body.pwd) {
+                // missing form fields
+                res.status(401);
+                res.render('login', {
+                  postback_url  : 'validate',
+                  action        : 'Sign in',
+                  
+                });
+            } else {
+                res.status(200)
+                    .render('consent', {
+              action        : 'consent',
+              postback_url  : 'consent',
+              req_scope     : "READ WRITE",
+              appName       : "TEST APP",
+                });
+            }
+            
+        }
+});
 
 
-  app.post('/login', (req, res) => {
-    // Insert Login Code Here
-    let username = req.body.username;
-    let password = req.body.password;
+app.post('/validate', (req,res)=>{
 
-    if (username === "admin" && password === "admin"){
-        res.status(200).send('login success');
+    if (req.body.submit != 'yes') {
+        console.log('user has declined to login');
+        // ! request.body.redirect_uri.startsWith('oob') &&
+        // ! request.body.redirect_uri.startsWith('urn:ietf:wg:oauth:2.0:oob')
+        console.log('BODY: ' + JSON.stringify(req.body.email));
+        res.render('validate');
+
+        // res.status(302)
+        //   .header('Location', req.body.redirect_uri + '?error=access_denied')
+        //   .end();
+        // return;
+      }
+    else{
+        console.log("sign ");
+        console.log(req.body.email);
+        console.log(req.body.pwd);
+        if ( ! req.body.email || ! req.body.pwd) {
+            // missing form fields
+            res.status(401);
+            res.render('login', {
+              postback_url  : 'validate',
+              action        : 'Sign in',
+              
+            });
+        } else {
+            res.status(200)
+                .render('consent', {
+          action        : 'consent',
+          postback_url  : 'consent',
+          req_scope     : "READ WRITE",
+          appName       : "TEST APP",
+            });
+        }
+        
     }
-    res.status(400).send('login failed');
-  });
+    // console.log('BODY: ' + JSON.stringify(req.body.email));
+});
 
-  const port = 3000 // Port we will listen on
-
-// Function to listen on the port
-app.listen(port, () => console.log(`This app is listening on port ${port}`));
+app.listen(3000);
